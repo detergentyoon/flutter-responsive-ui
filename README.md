d95d95d1d4a9e9cd8f8a6cad834ba0b92312b6ae
Eugene Cha <detergentyoon@gmail.com>
2023-07-17 23:59:08 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flutter에서의 반응형 UI 구현
+
+![img.png](info/img.png)
---
+++ android/app/src/main/kotlin/com/example/albert_camus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.albert_camus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
